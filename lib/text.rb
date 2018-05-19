89e0296481baa3710dcfe364d2e9e2eeeaa34eba
JP Lynch <JPLynch35@gmail.com>
2018-05-19 13:42:20 -0600

    Add space to front of text for terminal formatting

--- lib/text.rb
+++ lib/text.rb
@@ -18,11 +18,11 @@ module Text
 
   def player_ships_prompt
     puts(
-      "I have laid out my ships on the grid.
+      "      I have laid out my ships on the grid.
       You now need to layout your two ships.
       The first is two units long and the
       second is three units long.
-      The grid has A1 at the top left and D4 at the bottom right.\n\n
+      The grid has A1 at the top left and D4 at the bottom right.\n
       Enter the squares for the two-unit ship:"
     )
   end
