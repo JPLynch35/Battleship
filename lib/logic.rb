99bbec82c355c75a47d1938632ef5763b9b4ea7a
JP Lynch <JPLynch35@gmail.com>
2018-05-19 13:36:04 -0600

    Transfer game menu interaction from logic module to player class

--- lib/logic.rb
+++ lib/logic.rb
@@ -1,18 +1,7 @@
 require './lib/text'
 
 module Logic
-  include Text
+  
 
-  def player_menu_choice
-    loop do
-      user_menu_input = gets.chomp.downcase
-      if user_menu_input == "p" || user_menu_input == "play"
-        break
-      elsif user_menu_input == "i" || user_menu_input == "instructions"
-        game_instructions
-      elsif user_menu_input == "q" || user_menu_input == "quit"
-        exit
-      end
-    end
-  end
+
 end
