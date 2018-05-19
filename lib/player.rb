723702e017b53f6c9cbce75ae5e0d19826d9ca8e
JP Lynch <JPLynch35@gmail.com>
2018-05-19 13:41:34 -0600

    Add game menu to player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,20 @@
-module Player
+class Player
+
+  def initialize
+    @rounds = 0
+  end
+
+  def menu_choice
+    loop do
+      user_menu_input = gets.chomp.downcase
+      if user_menu_input == "p" || user_menu_input == "play"
+        break
+      elsif user_menu_input == "i" || user_menu_input == "instructions"
+        game_instructions
+      elsif user_menu_input == "q" || user_menu_input == "quit"
+        exit
+      end
+    end
+  end
 
 end
