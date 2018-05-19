b338b4d0bd08f8b15c1f6a486da1ae8df9270c4c
JP Lynch <JPLynch35@gmail.com>
2018-05-19 11:17:46 -0600

    Add computer set 3 ship

--- lib/board.rb
+++ lib/board.rb
@@ -1,15 +1,14 @@
 class Board
-  attr_reader :base,
-              :game_board,
+  attr_reader :grid,
               :grid_rules_3ship,
               :grid_rules_2ship
 
   def initialize
     @grid = {
-      'A1' => '', 'A2' => '', 'A3' => '', 'A4' => '',
-      'B1' => '', 'B2' => '', 'B3' => '', 'B4' => '',
-      'C1' => '', 'C2' => '', 'C3' => '', 'C4' => '',
-      'D1' => '', 'D2' => '', 'D3' => '', 'D4' => ''
+      'A1' => ' ', 'A2' => ' ', 'A3' => ' ', 'A4' => ' ',
+      'B1' => ' ', 'B2' => ' ', 'B3' => ' ', 'B4' => ' ',
+      'C1' => ' ', 'C2' => ' ', 'C3' => ' ', 'C4' => ' ',
+      'D1' => ' ', 'D2' => ' ', 'D3' => ' ', 'D4' => ' '
     }
     @grid_rules_3ship = {
       'A1' => ['d','r'], 'A2' => ['d','r'], 'A3' => ['d','l'], 'A4' => ['d','l'],
@@ -23,19 +22,21 @@ class Board
       'C1' => ['u','d','r'], 'C2' => ['u','d','r','l'], 'C3' => ['u','d','r','l'], 'C4' => ['u','d','l'],
       'D1' => ['u','r'], 'D2' => ['u','l','r'], 'D3' => ['u','l','r'], 'D4' => ['u','l']
     }
-    @game_board =
-      puts "==========="
-      puts ". 1 2 3 4"
-      puts "A #{@base['A1']} #{@base['A2']} #{@base['A3']} #{@base['A4']}"
-      puts "B #{@base['B1']} #{@base['B2']} #{@base['B3']} #{@base['B4']}"
-      puts "C #{@base['C1']} #{@base['C2']} #{@base['C3']} #{@base['C4']}"
-      puts "D #{@base['D1']} #{@base['D2']} #{@base['D3']} #{@base['D4']}"
-      puts "==========="
+  end
+
+  def game_board(board)
+    puts "==========="
+    puts ". 1 2 3 4"
+    puts "A #{board.grid['A1']} #{board.grid['A2']} #{board.grid['A3']} #{board.grid['A4']}"
+    puts "B #{board.grid['B1']} #{board.grid['B2']} #{board.grid['B3']} #{board.grid['B4']}"
+    puts "C #{board.grid['C1']} #{board.grid['C2']} #{board.grid['C3']} #{board.grid['C4']}"
+    puts "D #{board.grid['D1']} #{board.grid['D2']} #{board.grid['D3']} #{board.grid['D4']}"
+    puts "==========="
   end
 
   def set_3ship(board, all_ship_cells)
-    all_ship_cells
-
-
-
+    all_ship_cells.each do |cell|
+      board.grid[cell] = "S"
+    end
+  end
 end
