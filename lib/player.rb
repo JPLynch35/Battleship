93de5a545a6a06d7dbc1b70075f8989bc7b8e3d0
JP Lynch <JPLynch35@gmail.com>
2018-05-21 16:02:43 -0600

    Rename ship cell variable names

--- lib/player.rb
+++ lib/player.rb
@@ -37,17 +37,17 @@ class Player
   def input_3ship(board, p_endpoints_for_2ship)
     puts player_3ship_prompt
     p_endpoints_for_3ship = ""
-    p_cells_for_3ship = ""
+    p_3ship_cells = ""
     loop do
       p_endpoints_for_3ship = gets.chomp.upcase
-      p_cells_for_3ship = p_calculate_3ship_second_cell(p_endpoints_for_3ship)
-      valid_1 = p_check_3ship_is_possible(board, p_cells_for_3ship)
+      p_3ship_cells = p_calculate_3ship_second_cell(p_endpoints_for_3ship)
+      valid_1 = p_check_3ship_is_possible(board, p_3ship_cells)
       valid_2 = p_check_3ship_issues(board, p_endpoints_for_3ship, p_endpoints_for_2ship)
       if valid_1 == true && valid_2 == true
         break
       end
     end
-    return p_cells_for_3ship
+    return p_3ship_cells
   end
 
   def input_shot(board, shots_fired)
@@ -57,7 +57,7 @@ class Player
       if board.grid.has_key?(shot) == false
         puts shot_not_on_grid
       elsif @total_shots.include?(shot)
-        puts already_shot 
+        puts already_shot
       else
         @total_shots << shot
         puts valid_shot
