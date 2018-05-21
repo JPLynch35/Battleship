adb21d832bceefed066b02b8377a90e2e1f17b40
JP Lynch <JPLynch35@gmail.com>
2018-05-21 10:56:00 -0600

    Change line location for finish time

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -1,4 +1,5 @@
 require './lib/board'
+require './lib/computer'
 require './lib/player'
 require './lib/ship'
 require './lib/ship_validator'
@@ -19,39 +20,40 @@ c_cells_for_3ship = create_3ship(c_board)
 c_board.set_ship(c_cells_for_3ship)
 c_cells_for_2ship = create_2ship(c_board, c_cells_for_3ship)
 c_board.set_ship(c_cells_for_2ship)
-c_board.game_board #can delete this line, just showing computer board for testing
+# c_board.game_board #can delete this line, just showing computer board for testing
 
 p_board = Board.new
 p_cells_for_2ship = player.input_2ship(p_board)
 p_board.set_ship(p_cells_for_2ship)
 p_cells_for_3ship = player.input_3ship(p_board, p_cells_for_2ship)
 p_board.set_ship(p_cells_for_3ship)
-p_board.game_board #can delete this line, just showing computer board for testing
+# p_board.game_board #can delete this line, just showing computer board for testing
 
 loop do
   c_board.opposing_shots_board
   puts fire_prompt
   shot = player.input_shot(c_board, player.total_shots)
-  hits = c_board.check_hits(shot)
+  c_board.check_hits(shot)
   c_board.opposing_shots_board
+  hits = c_board.count_hits
   p_win = c_board.check_for_win(hits)
-  if p_win == 1
+  if p_win == true
     puts win
-    finish = Time.now
     break
   end
 
-  shot = computer.randomiz_shot(p_board)
-  hits = p_board.check_hits(shot)
+  shot = computer.randomize_shot(p_board)
+  p_board.check_hits(shot)
   p_board.opposing_shots_board
+  hits = p_board.count_hits
   c_win = p_board.check_for_win(hits)
-  if c_win == 1
+  if c_win == true
     puts lose
-    finish = Time.now
     break
   end
 end
 
+finish = Time.now
 time = p_board.time_check(start, finish)
 puts shots_to_finish(player.total_shots.length)
 puts time_to_finish(time)
